d5e288d39e7b91f7e9bb5ac1dbf30cc5d030c895
LennyHirsch <lenny.hirsch@gmail.com>
2024-03-30 19:15:09 +0000

    Update

--- tomasino.py
+++ tomasino.py
@@ -51,16 +51,22 @@ power = lambda x: 1346*(x**-2.373) + 3.34
 parsons = pd.read_csv('C:/Users/2090496H/OneDrive - University of Glasgow/Documents/Python/THz-TDS-Frequency-Response/Parsons.csv')
 
 # INITIALISE ARRAYS AND CONSTANTS
-Freq_response = []
+Freq_response_delta = []
+Freq_response_long = []
+Freq_response_short = []
 E_field = []
-E_det = []
+E_det_delta = []
+E_det_long = []
+E_det_short = []
 
-res = int(1e5)
+res = int(1e4)
 z = np.linspace(0,100e-9,res) # NOTE: This variable is a bit of a mystery...
-freq = np.linspace(0.1e12,5e12,res)
+freq = np.linspace(0.1e12,1e13,res)
 
 t_pump = 245e-15
-t_probe = 245e-15
+t_probe_delta = 1e-21
+t_probe_short = 55e-15
+t_probe_long = 245e-15
 
 # CALCULATING N_THZ: THIS IS DONE FROM A FITTING EQUATION OF EXPERIMENTAL DATA
 x = np.linspace(min(parsons['wl']), max(parsons['wl']), res)
@@ -68,19 +74,48 @@ n_thz = power(x) # this gives us values of n_thz for all points simulated. R-squ
 
 for i, f in enumerate(freq):
     delta_k = k(wvl_probe) + k(wvl(f)) - k(wvl_probe - wvl(f))
-    FR = freq_response(Aopt(omega(f), t_probe), X2, deltaPhi(L_det, delta_k))
+    FR_delta = freq_response(Aopt(omega(f), t_probe_delta), X2, deltaPhi(L_det, delta_k))
+    FR_long = freq_response(Aopt(omega(f), t_probe_long), X2, deltaPhi(L_det, delta_k))
+    FR_short = freq_response(Aopt(omega(f), t_probe_short), X2, deltaPhi(L_det, delta_k))
     EF = E(n_thz[i], omega(f), z[i])
 
-    Freq_response.append(FR)
+    Freq_response_delta.append(FR_delta)
+    Freq_response_long.append(FR_long)
+    Freq_response_short.append(FR_short)
+    
     E_field.append(EF**2)
-    E_det.append(FR*(EF**2))
+    # E_det_delta.append(FR_delta*(EF**2))
+    # E_det_long.append(FR_long*(EF**2))
+    # E_det_short.append(FR_short*(EF**2))
+
+# max_delta = max(Freq_response_delta)
+# max_long = max(Freq_response_long)
+# max_short = max(Freq_response_short)
+
+# Freq_response_delta = Freq_response_delta / max_delta
+# Freq_response_long = Freq_response_long / max_long
+# Freq_response_short = Freq_response_short / max_short
+
+E_det_delta = [f*e for f,e in zip(Freq_response_delta, E_field)]
+E_det_long = [f*e for f,e in zip(Freq_response_long, E_field)]
+E_det_short = [f*e for f,e in zip(Freq_response_short, E_field)]
 
 # CALCULATE TIME DOMAIN
 nfft = 8 # smooths out FFT
 sampleSpacing = freq[2] - freq[1]
 
-td = fft.fft(np.real(E_det), res*nfft)
+td_delta = fft.fft(np.real(E_det_delta), res*nfft)
+td_long = fft.fft(np.real(E_det_long), res*nfft)
+td_short = fft.fft(np.real(E_det_short), res*nfft)
 xt = fft.fftfreq(res*nfft, sampleSpacing)
+
+max_delta = max(td_delta)
+max_long = max(td_long)
+max_short = max(td_short)
+
+td_delta = td_delta / max_delta
+td_long = td_long / max_long
+td_short = td_short / max_short
 
 # PLOTTING E-FIELD
 plt.subplot(1,3,1)
@@ -91,7 +126,11 @@ plt.ylabel("E field ^2")
 
 # PLOTTING DETECTED E-FIELD
 plt.subplot(1,3,2)
-plt.plot(freq, np.abs(np.real(E_det)))
+plt.plot(freq, np.abs(np.real(E_det_delta)))
+plt.plot(freq, np.abs(np.real(E_det_long)))
+plt.plot(freq, np.abs(np.real(E_det_short)))
+plt.yscale('log')
+plt.legend(['Delta','Long','Short'])
 plt.title("Detected E-field (Frequency domain)")
 plt.xlabel("Freq")
 plt.ylabel("E field ^2")
@@ -99,7 +138,10 @@ plt.ylabel("E field ^2")
 # PLOTTING TIME-DOMAIN
 xlim = 0.5e-11
 plt.subplot(1,3,3)
-plt.plot(xt, np.real(td))
+plt.plot(xt, np.real(td_delta))
+plt.plot(xt, np.real(td_long))
+plt.plot(xt, np.real(td_short))
+plt.legend(['Delta','Long','Short'])
 plt.xlim([0,xlim])
 plt.title("THz pulse (time domain)")
 plt.xlabel("Time")
