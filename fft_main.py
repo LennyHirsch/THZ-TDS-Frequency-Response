b22d89eda8b4c42fd8a8dc0ec674955a3842779d
Lenny <lenny.hirsch@gmail.com>
2024-05-25 19:14:38 +0100

    Updates

--- fft_main.py
+++ fft_main.py
@@ -1,19 +1,29 @@
 import numpy as np
 from scipy.integrate import cumulative_trapezoid as cum_trap
-from scipy.optimize import minimize
-from matplotlib import pyplot as plt
-from fft_lib import *
+from fft_lib import (
+    import_thz,
+    pos2time,
+    normalise,
+    normalise_time,
+    pad_sig,
+    pad_time,
+    fft_thz,
+    plotter,
+    gaussian,
+    width,
+)
 
 # IMPORT RAW DATA
-path = './experimental-data/'
-filenames = \
-['datastruct_uncompressed - 23-11-14.csv',
-'datastruct_uncompressed - prelim 1 - 23-11-14.csv',
-'datastruct_uncompressed - 23-06-23-unpurged.csv',
-'datastruct_uncompressed - 1299 chop - 23-06-24.csv',
-'datastruct_uncompressed - 22-07-11.csv',
-'datastruct_uncompressed - 22-07-11.csv',
-'datastruct_compressed - 23-05-02.csv']
+path = "./experimental-data/"
+filenames = [
+    "datastruct_uncompressed - 23-11-14.csv",
+    "datastruct_uncompressed - prelim 1 - 23-11-14.csv",
+    "datastruct_uncompressed - 23-06-23-unpurged.csv",
+    "datastruct_uncompressed - 1299 chop - 23-06-24.csv",
+    "datastruct_uncompressed - 22-07-11.csv",
+    "datastruct_uncompressed - 22-07-11.csv",
+    "datastruct_compressed - 23-05-02.csv",
+]
 
 fullpath = path + filenames[2]
 
@@ -24,7 +34,7 @@ normalised_sig = normalise(sig)
 zeroed_time = normalise_time(time, sig)
 
 pad = 500
-step_size = pos[1]-pos[0]
+step_size = pos[1] - pos[0]
 
 # SET PARAMETERS FOR GAUSSIAN
 # COMPRESSED
@@ -63,25 +73,42 @@ padded_time = np.array(pad_time(zeroed_time, pad, start, end, step_size))
 # APPLY GAUSSIAN
 
 gauss = normalise(gaussian(padded_time, t0, width(tau)))
-gauss = gauss*factor
+gauss = gauss * factor
 
-sig_gauss = padded_sig*(1 + gauss)
+sig_gauss = padded_sig * (1 + gauss)
 
 cum_sum = normalise(cum_trap(sig_gauss, padded_time))
 
 print(f"Cumulative sum: {cum_sum[-1]}")
-print(".") # just here to keep result in terminal after execution
+print(".")  # just here to keep result in terminal after execution
 
 # PLOT COMPARISON OF SIGNALS (TIME-DOMAIN)
-plotter(padded_time[0:-1], padded_sig[0:-1], sig_gauss[0:-1], cum_sum, title='Comparing padded sig, sig_gauss, and cum sum', legend=['Padded sig', 'Sig gauss', 'Cum sum'])
+plotter(
+    padded_time[0:-1],
+    padded_sig[0:-1],
+    sig_gauss[0:-1],
+    cum_sum,
+    title="Comparing padded sig, sig_gauss, and cum sum",
+    legend=["Padded sig", "Sig gauss", "Cum sum"],
+)
 
 # FFT
 (orig_fft, freq) = fft_thz(padded_time, padded_sig)
 (fft, freq) = fft_thz(padded_time, sig_gauss)
-orig_fft = normalise(np.abs(orig_fft)**2)
-fft = normalise(np.abs(fft)**2)
+orig_fft = normalise(np.abs(orig_fft) ** 2)
+fft = normalise(np.abs(fft) ** 2)
 
-xlim = [-1, 10] # in THz
+xlim = [-1, 10]  # in THz
 ylim = [9e-5, 2]
 
-plotter(freq*10**-12, orig_fft, fft, title='Comparing FFTs', xlabel="Frequency (THz)", legend=['Original', 'Modified'], xlim=xlim, ylim=ylim, log=True)
+plotter(
+    freq * 10**-12,
+    orig_fft,
+    fft,
+    title="Comparing FFTs",
+    xlabel="Frequency (THz)",
+    legend=["Original", "Modified"],
+    xlim=xlim,
+    ylim=ylim,
+    log=True,
+)
--- wip.py
+++ wip.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import scipy.fft as fft
 from tomasino_fns_wip import *
 
-wvl_probe = 1030e-9 # probe wavelength: 1030 nm
-freq_thz = 3e12 # THz frequency: 3 THz
+wvl_probe = 1030e-9  # probe wavelength: 1030 nm
+freq_thz = 3e12  # THz frequency: 3 THz
 
 # SETTING SIMULATION PARAMETERS
 res = int(1e4)
-z = np.linspace(0,1e-3,res, dtype=np.float128)
-freq = np.linspace(9e11,1e13,res, dtype=np.float128)
+z = np.linspace(0, 1e-3, res, dtype=np.float128)
+freq = np.linspace(0.1e11, 1e13, res, dtype=np.float128)
 
-t_pump = 245e-15
-t_pp = t_pump / 2*np.sqrt(np.log(2))
+t_pump = 200e-15
+t_pp = t_pump / 2 * np.sqrt(np.log(2))
 print(f"t_pp: {t_pp}")
-t_probe = 245e-15
+t_probe = 1e-15
 print(f"t_probe: {t_probe}")
 
 # CALCULATING N_THZ: THIS IS DONE FROM A FITTING EQUATION OF EXPERIMENTAL DATA
@@ -23,17 +23,19 @@ x = np.linspace(wvl(min(freq)), wvl(max(freq)), res)
 n_thz = np.sqrt([eps_thz(omega(f)) for f in freq])
 
 # CALCULATE ALL THE THINGS
-(Ef, f, fp, foc, ovr, E_det, dk) = transfer_function(freq, wvl_probe, t_probe, t_pp, n_thz, z, 3.5e-3)
+(Ef, f, fp, foc, ovr, E_det, dk) = transfer_function(
+    freq, wvl_probe, t_probe, t_pp, n_thz, z, 3.5e-3
+)
 
 # CALCULATE TIME DOMAIN
-nfft = 8 # smooths out FFT
+nfft = 8  # smooths out FFT
 sampleSpacing = freq[2] - freq[1]
 
 # CALCULATE POWER SPECTRUM
 E_pow = [e**2 for e in np.abs(E_det)]
 
-td = fft.fft(np.abs(Ef), res*nfft)
-xt = fft.fftfreq(res*nfft, sampleSpacing)
+td = fft.fft(np.abs(Ef), res * nfft)
+xt = fft.fftfreq(res * nfft, sampleSpacing)
 
 # NORMALISING TO 1
 td = normalise(td)
@@ -56,16 +58,16 @@ E_pow = normalise(E_pow)
 # PLOTTING DETECTED E-FIELD
 # plt.subplot(1,2,1)
 plt.plot(freq, np.abs(E_pow))
-plt.yscale('log')
+plt.yscale("log")
 plt.title("Detected E-field (Frequency domain)")
 plt.xlabel("Freq")
 plt.ylabel("E field ^2")
 starty = 0.8e-4
-endy = 10
-# plt.ylim([starty,endy])
+endy = 2
+plt.ylim([starty, endy])
 startx = 0
 endx = 1e13
-plt.xlim([startx,endx])
+plt.xlim([startx, endx])
 # plt.grid()
 
 # PLOTTING TIME-DOMAIN
@@ -82,11 +84,15 @@ plt.xlim([startx,endx])
 # # plt.gca().set_position([0,0,1,1])
 # # plt.grid()
 #
-dir = "./figures/gallot-deltak-freqrespxEfield/"
-filename = "pump-" + str(int(t_pump*10**15)) + "fs_probe-" + str(int( t_probe*10**15 )) + "fs"
+dir = "./output/"
+filename = (
+    "pump-"
+    + str(int(t_pump * 10**15))
+    + "fs_probe-"
+    + str(int(t_probe * 10**15))
+    + "fs"
+)
 fullname = f"{dir}{filename}.svg"
-# print(fullname)
-# plt.savefig(fullname, format="svg")
-# plt.savefig(fullname)
+print(fullname)
+plt.savefig(fullname)
 plt.show()
-
